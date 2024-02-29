4088d319eff026e1957ef9efc9a9631917699c76
Your Name <austin.paxton007@gmail.com>
2024-02-28 20:37:02 -0800

    add calibration functions and tests

--- tests/test_misc_utils.py
+++ tests/test_misc_utils.py
@@ -1,5 +1,86 @@
-import os
-import sys
+import pytest
+import numpy as np
+from math import sqrt
 
-sys.path.append(os.path.abspath(".."))
-from utils.misc_utils import *
+from ..utils.misc_utils import *
+
+
+# pytest.mark.parameterize confused by np array (double counted params)
+# this is next best option
+class Test_find_rotation_matrix:
+    def test_find_rotation_matrix_case1(self):
+        # test 90 deg counter-clockwise rotation
+        assert np.allclose(
+            find_rotation_matrix([0, 1], [-1, 0]), np.array([[0, -1], [1, 0]])
+        )
+
+    def test_find_rotation_matrix_case2(self):
+        # test 90 deg clockwise rotation
+        assert np.allclose(
+            find_rotation_matrix([0, 1], [1, 0]), np.array([[0, 1], [-1, 0]])
+        )
+
+    def test_find_rotation_matrix_case3(self):
+        # test 90 deg counter-clockwise rotation w/ different src_vec
+        assert np.allclose(
+            find_rotation_matrix([1, 0], [0, 1]), np.array([[0, -1], [1, 0]])
+        )
+
+    def test_find_rotation_matrix_case4(self):
+        # test 90 deg clockwise rotation w/ different src_vec
+        assert np.allclose(
+            find_rotation_matrix([0, -1], [-1, 0]), np.array([[0, 1], [-1, 0]])
+        )
+
+    def test_find_rotation_matrix_45deg(self):
+        # 45 degree counter clock rotation
+        assert np.allclose(
+            find_rotation_matrix([1, 0], [1, 1]),
+            np.array([[1, -1], [1, 1]]),
+        )
+
+    def test_find_rotation_matrix_same_vec(self):
+        # test same src_vec and dest_vec case
+        assert np.allclose(
+            find_rotation_matrix([-1, -1], [-1, -1]), np.array([[1, 0], [0, 1]])
+        )
+
+    def test_find_rotation_matrix_same_vec_diff_magnitude(self):
+        # test same src_vec and dest_vec directions with different
+        # src_vec and dest_vec magnitudes
+        assert np.allclose(
+            find_rotation_matrix([2, 2], [1, 1]), np.array([[1, 0], [0, 1]])
+        )
+
+
+def test_delta_vec():
+    vec1 = [1, 2]
+    vec2 = [-1, 4]
+    assert delta_vec(*vec1, *vec2) == (2, -2)
+
+
+def test_calibrate_rotation_and_translation():
+    """
+    test calibrate function n scenario where rotation and transation of points is required
+    """
+    src_pt1 = [-1, 0]
+    src_pt2 = [1, 0]
+    out_pt1 = [1, 1]
+    out_pt2 = [1, 3]
+    rotation_matrix, delta_translation = calibrate(src_pt1, src_pt2, out_pt1, out_pt2)
+    assert np.allclose(rotation_matrix, np.array([[0, -1], [1, 0]]))
+    assert np.allclose(delta_translation, np.array([1, 2]))
+
+
+def test_convert_point_to_new_coord_system():
+    """
+    test in scenario where rotation and transation of points is required
+    """
+    rotation_matrix = np.array([[0, -1], [1, 0]])
+    delta_translation = np.array([1, 2])
+    src_pt1 = [-1, 0]
+    out_pt1 = [1, 1]
+    assert np.allclose(
+        convert_point_to_new_coord_system(*src_pt1, rotation_matrix, delta_translation),
+        out_pt1,
+    )
--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -1,33 +1,64 @@
 import numpy as np
+from icecream import ic
+import pandas as pd
 
 
-def rotation_matrix_from_2d_vectors(vec1, vec2):
+def find_rotation_matrix(src_vec, dest_vec):
     """
-    Find the rotation matrix that aligns vec1 to vec2 in 2D.
-    :param vec1: A 2d "source" vector
-    :param vec2: A 2d "destination" vector
-    :return: A transform matrix (2x2) which, when applied to vec1, aligns it with vec2.
+    Find the rotation matrix that aligns src_vec to dest_vec in 2D.
+    :param src_vec: A 2d "source" vector
+    :param dest_vec: A 2d "destination" vector
+    :return: A transform matrix (2x2) which, when applied to src_vec, aligns it with dest_vec.
     """
     # Normalize the vectors
-    a = vec1 / np.linalg.norm(vec1)
-    b = vec2 / np.linalg.norm(vec2)
+    a = src_vec / np.linalg.norm(src_vec)
+    b = dest_vec / np.linalg.norm(dest_vec)
 
-    # Compute the cosine and sine of the angle between vec1 and vec2
+    # Compute the cosine and sine of the angle between src_vec and dest_vec
     cos_theta = np.dot(a, b)
     sin_theta = np.linalg.det(np.array([a, b]))
+    print(sin_theta)
 
     # Construct the rotation matrix
     rotation_matrix = np.around(
         np.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]])
     )
-
+    print(rotation_matrix)
     return rotation_matrix
 
 
-vec1 = [0, 1]
-vec2 = [-1, 0]
+def matrix_rotation(x, y, rotation_matrix):
+    input_vec = np.array([[x], [y]])
+    rotated_vec = rotation_matrix @ input_vec
+    return [num for num in rotated_vec.flat]
 
-rot_mat = rotation_matrix_from_2d_vectors(vec1, vec2)
 
-print(rot_mat)
-print(type(rot_mat))
+def delta_vec(x1: float, y1: float, x2: float, y2: float) -> list:
+    """
+    Finds resulting delta vector between 2 points
+    """
+    return x1 - x2, y1 - y2
+
+
+def calibrate(src_pt1: list, src_pt2: list, out_pt1: list, out_pt2) -> tuple:
+    """
+    Find rotation matrix and translation delta to convert source points (src_pt)
+    to output points (out_pt)
+    """
+    # use delta vectors bc if model is rotated we care about relative distances between points
+    delta_vec_src = delta_vec(*src_pt1, *src_pt2)
+    delta_vec_out = delta_vec(*out_pt1, *out_pt2)
+    rotation_matrix = find_rotation_matrix(delta_vec_src, delta_vec_out)
+    src_pt1_rot = matrix_rotation(*src_pt1, rotation_matrix)
+    delta_translation = np.array(delta_vec(*out_pt1, *src_pt1_rot))
+    ic(delta_translation)
+    ic(rotation_matrix)
+    return rotation_matrix, delta_translation
+
+
+def convert_point_to_new_coord_system(x, y, rotation_matrix, delta_translation):
+    return (matrix_rotation(x, y, rotation_matrix) + delta_translation).tolist()
+
+
+if __name__ == "__main__":
+    pass
